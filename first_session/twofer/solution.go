36f28064a1927eb2583170157d2d706f1bd054d6
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-07-30 20:15:05 +0330

    add: twofer

--- first_session/twofer/solution.go
+++ first_session/twofer/solution.go
@@ -1,1 +1,19 @@
 package twofer
+
+import "fmt"
+
+func twoFer(name string) string {
+	if name == "" {
+		return "One for you, one for me."
+	}
+
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
+
+func main() {
+	// Test cases
+	fmt.Println(twoFer("Alice"))  // Output: One for Alice, one for me.
+	fmt.Println(twoFer("Bohdan")) // Output: One for Bohdan, one for me.
+	fmt.Println(twoFer(""))       // Output: One for you, one for me.
+	fmt.Println(twoFer("Zaphod")) // Output: One for Zaphod, one for me.
+}
