f4e1eadb1a469a2b928eeb65e001773c3089a455
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-07-31 17:24:47 +0330

    add difference of squares

---
+++ first_session/Difference-of-Squares/solution.go
@@ -0,0 +1,23 @@
+package differenceofsquares
+
+import "fmt"
+
+func main() {
+	N := 10
+
+	sum := 0
+	for i := 1; i <= N; i++ {
+		sum += i
+	}
+
+	squareOfSum := sum * sum
+
+	sumOfSquares := 0
+	for i := 1; i <= N; i++ {
+		sumOfSquares += i * i
+	}
+
+	difference := squareOfSum - sumOfSquares
+
+	fmt.Printf("The difference between the square of the sum and the sum of the squares of the first %d natural numbers is %d\n", N, difference)
+}
