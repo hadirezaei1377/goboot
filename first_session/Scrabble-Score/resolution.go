9d7f531b073581cb2aae9b998acb64ddeed33a28
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-03 15:31:47 +0330

    resolve scrabble score

--- first_session/Scrabble-Score/resolution.go
+++ first_session/Scrabble-Score/resolution.go
@@ -1,1 +1,36 @@
 package scrabblescore
+
+import "strings"
+
+func getLetterValue(letter rune) int {
+	switch letter {
+	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+		return 1
+	case 'D', 'G':
+		return 2
+	case 'B', 'C', 'M', 'P':
+		return 3
+	case 'F', 'H', 'V', 'W', 'Y':
+		return 4
+	case 'K':
+		return 5
+	case 'J', 'X':
+		return 8
+	case 'Q', 'Z':
+		return 10
+	default:
+		return 0
+	}
+}
+
+func calculateScrabbleScore(word string) int {
+
+	word = strings.ToUpper(word)
+	totalScore := 0
+
+	for _, letter := range word {
+		totalScore += getLetterValue(letter)
+	}
+
+	return totalScore
+}
