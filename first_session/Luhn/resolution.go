82a7df7982e31677ec4663ac60eab64332ab3bff
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-03 22:08:27 +0330

    resolve luhn

--- first_session/Luhn/resolution.go
+++ first_session/Luhn/resolution.go
@@ -1,1 +1,32 @@
 package luhn
+
+import "strings"
+
+func IsValid(id string) bool {
+	id = strings.Replace(id, " ", "", -1)
+
+	if len(id) < 2 {
+		return false
+	}
+
+	sum := 0
+
+	for i := len(id) - 1; i >= 0; i-- { // start from last
+		if id[i] < '0' || id[i] > '9' { // check if the character is a digit
+			return false
+		}
+
+		a := int(id[i] - '0')
+
+		if (len(id)-1-i)%2 != 0 { // double every second digit from the right
+			a *= 2
+			if a > 9 {
+				a -= 9
+			}
+		}
+
+		sum += a
+	}
+
+	return sum%10 == 0
+}
