2757857d742d0164572320db7a9d22ab5c1c864e
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-06 12:31:22 +0330

    add rnatranscription

--- second_session/RNA_Transcription/solution.go
+++ second_session/RNA_Transcription/solution.go
@@ -1,3 +1,18 @@
 package rnatranscription
 
-// comment it when you are resolving
+func ToRNA(dna string) string {
+	retVal := ""
+	for _, ch := range dna {
+		switch ch {
+		case 'G':
+			retVal += "C"
+		case 'C':
+			retVal += "G"
+		case 'T':
+			retVal += "A"
+		case 'A':
+			retVal += "U"
+		}
+	}
+	return retVal
+}
