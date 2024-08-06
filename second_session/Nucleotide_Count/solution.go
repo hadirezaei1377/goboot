65e99585151692715dd55ad8b85e27cafce152ff
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-06 12:20:45 +0330

    add nucleotidecount

--- second_session/Nucleotide_Count/solution.go
+++ second_session/Nucleotide_Count/solution.go
@@ -1,3 +1,29 @@
 package nucleotidecount
 
-// comment it when you are resolving
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Histogram map[rune]int
+
+type DNA string
+
+const nucleotides = "ACGT"
+
+func (d DNA) Counts() (Histogram, error) {
+	h := Histogram{
+		'A': 0,
+		'C': 0,
+		'G': 0,
+		'T': 0,
+	}
+	for _, r := range d {
+		if !strings.Contains(nucleotides, string(r)) {
+			return nil, errors.New(fmt.Sprintf("Invalid nucleotide: %v", r))
+		}
+		h[r] += 1
+	}
+	return h, nil
+}
