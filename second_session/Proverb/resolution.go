69ce99fced0ba22505545bc1d870932b3747c379
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-10 23:15:15 +0330

    resolve Proverb

--- second_session/Proverb/resolution.go
+++ second_session/Proverb/resolution.go
@@ -1,1 +1,28 @@
 package proverb
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	items := []string{"میخ", "کفش", "اسب", "سوار", "پیام", "نبرد", "پادشاهی"}
+
+	proverb := CreateProverb(items)
+
+	fmt.Println(proverb)
+}
+
+func CreateProverb(items []string) string {
+	var result []string
+
+	for i := 0; i < len(items)-1; i++ {
+		line := fmt.Sprintf("به دلیل نداشتن %s، %s گم شد.", items[i], items[i+1])
+		result = append(result, line)
+	}
+
+	lastLine := fmt.Sprintf("و همه اینها به خاطر نیاز به یک %s.", items[0])
+	result = append(result, lastLine)
+
+	return strings.Join(result, "\n")
+}
