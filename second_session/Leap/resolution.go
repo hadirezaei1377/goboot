5ce60971213df79dd2b5f599026d8e21f17c4665
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-10 23:06:13 +0330

    resolve leap

--- second_session/Leap/resolution.go
+++ second_session/Leap/resolution.go
@@ -1,1 +1,16 @@
 package leap
+
+import "fmt"
+
+func main() {
+	year := 2000
+
+	if year%4 == 0 {
+		if year%100 != 0 || (year%100 == 0 && year%400 == 0) {
+			fmt.Println(year, " yes ")
+		} else {
+			fmt.Println(year, " no ")
+		}
+	}
+
+}
--- second_session/Leap/solution.go
+++ second_session/Leap/solution.go
@@ -1,5 +1,9 @@
 package leap
 
+/*
+
 func IsLeapYear(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
+
+*/
