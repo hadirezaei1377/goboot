8188822c2b76f89d97b3b671059933458a136c26
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-29 00:01:58 +0330

    add more advanced sql

--- MySQL/sql.md
+++ MySQL/sql.md
@@ -9,3 +9,13 @@ add a new column to the table : ALTER TABLE - ADD COLUMN
 change the column type or name : ALTER TABLE - MODIFY COLUMN / CHANGE
 delete a column : ALTER TABLE - DROP COLUMN
 delete a table : DROP
+
+************************************************************************************************
+
+for creating an index for one or more columns to improve the search speed : CREATE INDEX
+delete the index : DROP INDEX
+join
+transactions
+aggregate functions : COUNT, AVG, MAX, MIN, SUM
+
+
--- MySQL/sql.sql
+++ MySQL/sql.sql
@@ -29,3 +29,47 @@ ALTER TABLE users CHANGE COLUMN name full_name VARCHAR(255);
 ALTER TABLE users DROP COLUMN email;
 
 DROP TABLE users;
+
+************************************************************************************************
+
+CREATE INDEX idx_name ON users (name);
+
+DROP INDEX idx_name ON users;
+
+                                                   ***
+inner join:
+SELECT users.name, orders.order_date
+FROM users
+INNER JOIN orders ON users.id = orders.user_id;
+
+left join:
+SELECT users.name, orders.order_date
+FROM users
+LEFT JOIN orders ON users.id = orders.user_id;
+
+right join:
+SELECT users.name, orders.order_date
+FROM users
+RIGHT JOIN orders ON users.id = orders.user_id;
+
+full join:
+SELECT users.name, orders.order_date
+FROM users
+FULL OUTER JOIN orders ON users.id = orders.user_id;
+
+cross join:
+SELECT users.name, orders.order_date
+FROM users
+CROSS JOIN orders;
+
+transaction:
+START TRANSACTION;
+
+INSERT INTO users (name, phone_number) VALUES ('Alice', '09991234567');
+UPDATE users SET phone_number = '0987654321' WHERE name = 'Bob';
+
+COMMIT;  
+
+
+ROLLBACK; 
+
